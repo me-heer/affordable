434fbdeb8c594682b5fd4b5f44cdc68c85d92682
Mihir Joshi <mihir.joshi@senpiper.com>
2022-05-07 18:34:31 +0530

    Temp Commit

--- flipkart/flipkartScript.js
+++ flipkart/flipkartScript.js
@@ -1,12 +1,9 @@
-// TODO: Update when page URL is updated
-// TODO: Listen to extension updates
-
 const blacklistedClasses = [];
 main();
 
 function main() {
     updateAllPrices();
-    setInterval(updateAllPrices, 1 * 1000);
+    setInterval(updateAllPrices, refreshRateInMillis);
 }
 
 function updateAllPrices() {
@@ -17,39 +14,18 @@ function appendMainPrice(element, appendContent) {
     element.append(appendContent);
 }
 
-function getAppendContent(productPrice, salary) {
-    let timeTakenToEarn = parseInt(productPrice * (30 / salary));
-    if (timeTakenToEarn === 0)
-        return ` (less than 1 day)`
-    if (timeTakenToEarn === 1)
-        return ` (1 day)`
-    return ` (${timeTakenToEarn} days)`
-}
 
 function updateMainPrice(className) {
     let elements = document.querySelectorAll(className);
-    console.log("Total Elements: " + elements.length)
 
     elements.forEach((element) => {
+
         let productPrice = currency(element.textContent).value;
-        if (!isNaN(productPrice) && productPrice !== 0 && !containsBlacklistedClasses(element)  && !isAlreadyAppended(element.textContent)) {
-            chrome.storage.sync.get("salary", ({salary}) => {
+        if (isValid(productPrice, element, blacklistedClasses)) {
+            getFromStorageSync("salary", ({salary}) => {
                 appendMainPrice(element, getAppendContent(productPrice, salary));
             });
         }
     });
 }
 
-function containsBlacklistedClasses(element) {
-    for (let blacklistedClass of blacklistedClasses) {
-        if (element.classList.contains(blacklistedClass))
-            return true;
-    }
-    return false;
-}
-
-
-function isAlreadyAppended(elementText) {
-    let result = elementText.includes('(') && elementText.includes(')')
-    return result;
-}
