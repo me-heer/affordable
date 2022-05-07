218e9a383326c948f5022488b7c39f45e72a1ee7
Mihir <mihir67mj@gmail.com>
2022-05-07 18:34:31 +0530

    Temp Commit

--- amazon/amazonScript.js
+++ amazon/amazonScript.js
@@ -1,28 +1,23 @@
-// TODO: Update when page URL is updated
-// TODO: Listen to extension updates
-
 const blacklistedClasses = ["a-row", "savingsPercentage"];
+
+const mainPrice = {
+    getter: ["firstChild", "textContent"],
+    setter: ["lastChild"]
+}
+
 main();
 
-chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
-        // listen for messages sent from background.js
 
-        if (request.message === 'updatePrices') {
-            updateAllPrices();
-        }
-    });
 
 
 function main() {
     updateAllPrices();
-    setInterval(updateAllPrices, 2 * 1000);
+    setInterval(updateAllPrices, refreshRateInMillis);
 }
 
 function updateAllPrices() {
     updateMainPrice(".a-price");
     updateColorPrice(".a-color-price");
-    // updateDealPrice(".a-price");
 }
 
 
@@ -34,66 +29,34 @@ function appendMainPrice(element, appendContent) {
     element.lastChild.append(appendContent);
 }
 
-function getAppendContent(productPrice, salary) {
-    let timeTakenToEarn = parseInt(productPrice * (30 / salary));
-    if (timeTakenToEarn === 0)
-        return ` (less than 1 day)`
-    if (timeTakenToEarn === 1)
-        return ` (1 day)`
-    return ` (${timeTakenToEarn} days)`
-}
 
 function updateMainPrice(className) {
     let elements = document.querySelectorAll(className);
-
     elements.forEach((element) => {
         let productPrice = currency(element.firstChild.textContent).value;
-        if (!isNaN(productPrice) && productPrice !== 0 && !containsBlacklistedClasses(element) && !isAlreadyAppended(element.textContent)) {
-            chrome.storage.sync.get("salary", ({salary}) => {
-                appendMainPrice(element, getAppendContent(productPrice, salary));
-            });
-        }
-    });
-}
 
-function containsBlacklistedClasses(element) {
-    for (let blacklistedClass of blacklistedClasses) {
-        if (element.classList.contains(blacklistedClass))
-            return true;
-    }
-    return false;
+        let testElement = element;
+        for (const getterKey of mainPrice.getter) {
+            testElement = testElement[getterKey.toString()]
+        }
+
+        if (isValid(productPrice, element, blacklistedClasses)) {
+            getFromStorageSync("salary", ({salary}) => {
+                appendMainPrice(element, getAppendContent(productPrice, salary));
+            })
+        }
+
+    });
 }
 
 function updateColorPrice(className) {
     let elements = document.querySelectorAll(className);
     elements.forEach((element) => {
         let productPrice = currency(element.textContent).value;
-        if (!isNaN(productPrice) && productPrice !== 0 && !containsBlacklistedClasses(element) && !isAlreadyAppended(element.textContent)) {
-            chrome.storage.sync.get("salary", ({salary}) => {
+        if (isValid(productPrice, element, blacklistedClasses)) {
+            getFromStorageSync("salary", ({salary}) => {
                 appendColorPrice(element, getAppendContent(productPrice, salary));
             });
         }
     });
-}
-
-function updateDealPrice(className) {
-    let elements = document.querySelectorAll(className);
-
-    elements.forEach((element) => {
-        let productPrice = currency(element.firstChild.textContent).value;
-        if (!isNaN(productPrice) && productPrice !== 0 && !containsBlacklistedClasses(element) && !isAlreadyAppended(element.firstChild.textContent)) {
-            chrome.storage.sync.get("salary", ({salary}) => {
-                appendMainPrice(element, getAppendContent(productPrice, salary));
-            });
-        }
-    });
-}
-
-function appendDealPrice(element, appendContent) {
-    element.lastChild.append(appendContent);
-}
-
-function isAlreadyAppended(elementText) {
-    let result = elementText.includes('(') && elementText.includes(')')
-    return result;
 }
