399a55ac58201e4bc7be6defaefdac880614e242
Mihir <mihir67mj@gmail.com>
2022-05-08 12:39:55 +0530

    Refactored updatePrice function so that both the scripts can use the same

--- amazon/amazonScript.js
+++ amazon/amazonScript.js
@@ -1,14 +1,19 @@
 const blacklistedClasses = ["a-row", "savingsPercentage"];
 
-const mainPrice = {
-    getter: ["firstChild", "textContent"],
-    setter: ["lastChild"]
-}
+const elements = [
+    {
+        className: ".a-price",
+        getter: ["firstChild", "textContent"],
+        setter: ["lastChild"]
+    },
+    {
+        className: ".a-color-price",
+        getter: ["textContent"],
+        setter: []
+    }
+]
 
 main();
-
-
-
 
 function main() {
     updateAllPrices();
@@ -16,47 +21,7 @@ function main() {
 }
 
 function updateAllPrices() {
-    updateMainPrice(".a-price");
-    updateColorPrice(".a-color-price");
-}
-
-
-function appendColorPrice(element, appendContent) {
-    element.append(appendContent);
-}
-
-function appendMainPrice(element, appendContent) {
-    element.lastChild.append(appendContent);
-}
-
-
-function updateMainPrice(className) {
-    let elements = document.querySelectorAll(className);
-    elements.forEach((element) => {
-        let productPrice = currency(element.firstChild.textContent).value;
-
-        let testElement = element;
-        for (const getterKey of mainPrice.getter) {
-            testElement = testElement[getterKey.toString()]
-        }
-
-        if (isValid(productPrice, element, blacklistedClasses)) {
-            getFromStorageSync("salary", ({salary}) => {
-                appendMainPrice(element, getAppendContent(productPrice, salary));
-            })
-        }
-
-    });
-}
-
-function updateColorPrice(className) {
-    let elements = document.querySelectorAll(className);
-    elements.forEach((element) => {
-        let productPrice = currency(element.textContent).value;
-        if (isValid(productPrice, element, blacklistedClasses)) {
-            getFromStorageSync("salary", ({salary}) => {
-                appendColorPrice(element, getAppendContent(productPrice, salary));
-            });
-        }
-    });
+    for (const elementInfo of elements) {
+       updatePrice(elementInfo, blacklistedClasses)
+    }
 }
