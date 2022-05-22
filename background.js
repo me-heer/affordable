2d339a52cb9b37f62f4df585b636b2c963bf10e8
Mihir Joshi <mihir67mj@gmail.com>
2022-05-22 13:20:56 +0530

    Improved Hover Mode
    - Added check so that hover mode is configurable

--- background.js
+++ background.js
@@ -1,11 +1,12 @@
-let salary = "48000";
-let hoverMode = true;
+let settings = {
+    salary: "30000",
+    hoverMode: true
+}
 
 chrome.runtime.onInstalled.addListener(setConfigs);
 
 function setConfigs() {
-    chrome.storage.sync.set({salary});
-    console.log(`Default Salary: ${salary}`);
-    chrome.storage.sync.set({hoverMode});
-    console.log(`Hover Mode: ${hoverMode}`);
+    chrome.storage.sync.set({settings});
+    console.log(`Default Salary: ${settings.salary}`);
+    console.log(`Hover Mode: ${settings.hoverMode}`);
 }
